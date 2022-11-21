b0e0f41c009c25fa026ffab7ad71a157e724c0af
Dadayablin <fiskar0408@gmail.com>
2022-11-22 02:08:50 +0300

    spam fix

--- utils/functions/spam.ts
+++ utils/functions/spam.ts
@@ -1,81 +1,58 @@
-import Say from "./Say";
 import { client } from "../../src/bot";
-import getUser from "./getUser";
-import invisChars from "../invisChars";
+const sett = require("../settings.json");
 
-const modsId = require("../mods.json");
-const prefix = "-";
-
-async function spam(msg: any) {
-  if (msg.args[1] == "i") {
-    if (msg.args[0] == "t") {
-      let time = Number(msg.args[2]) * 1000;
-      let mess = msg.args.splice(3).join(" ");
-      let inter = setInterval(() => {
-        Say(msg.channelName, 0, mess);
-        client.on("PRIVMSG", async (msg: any) => {
-          if (modsId.id.includes(msg.senderUserID)) {
-            msg.text = msg.messageText.replace(invisChars, "");
-            msg.args = msg.text.slice(prefix.length).trim().split(/ +/);
-            const command = msg.args.shift().toLowerCase();
-            if (command == "stop") {
-              clearInterval(inter);
-            }
-          }
-        });
-      }, time);
-    } else {
-      const user = await getUser(
-        msg.args[0] ? msg.args[0].replace("@", "") : msg.senderUsername
-      );
-      if (!user) {
-        return client.privmsg(
-          msg.channelName,
-          `Указанного пользователя не существует.`
+function spam(msg: any) {
+  if (!msg.args.length) {
+    return client.privmsg(msg.channelName, `Укажи фразу.`);
+  }
+  const channelCheck = msg.args.join(" ").match(/(channel)(:|=)(\w+)/i);
+  let channelTarget = channelCheck
+    ? channelCheck[3].toLowerCase()
+    : msg.channelName;
+  if (channelCheck) {
+    msg.args.splice(msg.args.indexOf(channelCheck[0]), 1);
+  }
+  const intCheck = msg.args.join(" ").match(/(i|int)(:|=)(\d+)/i);
+  let spamTime = intCheck ? intCheck[3] : 1060;
+  if (intCheck) {
+    msg.args.splice(msg.args.indexOf(intCheck[0]), 1);
+  }
+  const countCheck = msg.args.join(" ").match(/(c|count)(:|=)(\d+)/i);
+  let countAmount = countCheck ? countCheck[3] : 5;
+  if (countCheck) {
+    msg.args.splice(msg.args.indexOf(countCheck[0]), 1);
+  }
+  if (msg.args[0] === "--me") {
+    let i = 1;
+    let spammer = setInterval(() => {
+      if (i & 1) {
+        client.privmsg(channelTarget, `${msg.args.slice(1).join(" ")}`);
+      } else {
+        client.privmsg(
+          channelTarget,
+          `/me ${msg.args.slice(1).join(" ")} \u{E0000}`
         );
       }
-      let chan = msg.args[0];
-      let time = Number(msg.args[2]) * 1000;
-      let mess = msg.args.splice(3).join(" ");
-      let inter = setInterval(() => {
-        Say(chan, 0, mess);
-        client.on("PRIVMSG", async (msg: any) => {
-          msg.text = msg.messageText.replace(invisChars, "");
-          msg.args = msg.text.slice(prefix.length).trim().split(/ +/);
-          if (modsId.id.includes(msg.senderUserID)) {
-            const command = msg.args.shift().toLowerCase();
-            if (command == "stop") {
-              clearInterval(inter);
-            }
-          }
-        });
-      }, time);
-    }
-  } else {
-    if (msg.args[0] == "t") {
-      let num = Number(msg.args[1]);
-      let time = Number(msg.args[2]) * 1000;
-      let mess = msg.args.splice(3).join(" ");
-      for (let y = 0; y < num; y++) {
-        Say(msg.channelName, time * y, mess);
+      if (i++ == countAmount) clearInterval(spammer);
+    }, spamTime);
+    return;
+  }
+  if (msg.args[0] != "--stop") {
+    let i = 1;
+    let spammer = setInterval(() => {
+      sett.int = spammer;
+      if (i & 1) {
+        client.privmsg(channelTarget, `${msg.args.join(" ")}`);
+      } else {
+        client.privmsg(channelTarget, `${msg.args.join(" ")} \u{E0000}`);
       }
-    } else {
-      const user = await getUser(
-        msg.args[0] ? msg.args[0].replace("@", "") : msg.senderUsername
-      );
-      if (!user) {
-        return client.privmsg(
-          msg.channelName,
-          `Указанного пользователя не существует.`
-        );
-      }
-      let chan = msg.args[0];
-      let num = Number(msg.args[1]);
-      let time = Number(msg.args[2]) * 1000;
-      let mess = msg.args.splice(3).join(" ");
-      for (let y = 0; y < num; y++) {
-        Say(chan, time * y, mess);
-      }
+      if (i++ == countAmount) clearInterval(spammer);
+    }, spamTime);
+  }
+  if (msg.args[0] == "--stop") {
+    if (sett.int != null){
+      clearInterval(sett.int)
+      sett.int = null
     }
   }
 }
